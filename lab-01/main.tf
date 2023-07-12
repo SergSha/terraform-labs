15faafdeed1e9c027ae86b113a190f8c93a88bba
Serg Shagalov <sy.shagalov@yandex.ru>
2023-07-12 22:38:08 +0300

    modify main.tf

--- lab-01/main.tf
+++ lab-01/main.tf
@@ -56,3 +56,40 @@ resource "libvirt_cloudinit_disk" "commoninit" {
   pool = libvirt_pool.pool.name
 }
 
+resource "libvirt_domain" "server" {
+  name   = "server-example1"
+  memory = 1024
+  vcpu   = 2
+
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
+
+  network_interface {
+    network_name = libvirt_network.network.name
+  }
+
+  # IMPORTANT: this is a known bug on cloud images, since they expect a console
+  # we need to pass it
+  # https://bug.launchpad.net/cloud-images/+bug/1573095
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = libvirt_volume.server_root.id
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
+
